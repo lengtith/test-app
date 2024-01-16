a02f61c95663ba48f6e02b7eb0ac9217afda3964
lengtith <sotith1998@gmail.com>
2024-01-16 16:25:13 +0700

    Create header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <div className="flex items-center justify-between bg-gray-300 p-5 mb-5">
+        <div className="flex gap-4">
+          <Link to='/'>Welcome</Link>
+          <Link to='/products'>Products</Link>
+        </div>
+        <div className="flex items-center gap-4">
+          <Link to='/'>Login</Link>
+          <button className="px-3 py-2 rounded-lg bg-red-600 text-white">Logout</button>
+        </div>
+      </div>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
