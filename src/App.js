6cfc7c24994d44ca98af6d156d2c0472bf557793
lengtith <sotith1998@gmail.com>
2024-01-16 16:46:38 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { Link, Route, Routes } from 'react-router-dom';
 
@@ -20,21 +19,6 @@ function App() {
           <button className="px-3 py-2 rounded-lg bg-red-600 text-white">Logout</button>
         </div>
       </div>
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
 
       <Routes>
         <Route index path="/" element={<Welcome />} />
