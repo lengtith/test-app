e415ae8d9d64ace2f653a4a9f851eb9b7832c53b
lengtith <sotith1998@gmail.com>
2024-01-18 10:00:35 +0700

    update

--- src/pages/ProductUpdate.js
+++ src/pages/ProductUpdate.js
@@ -1,8 +1,56 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { useCookies } from 'react-cookie';
 
 const ProductUpdate = () => {
+  const [cookies] = useCookies(["user"]);
+  const [userInfo, setUserInfo] = useState({})
+  const [user, setUser] = useState({})
+
+  useEffect(() => {
+
+    const getUser = async () => {
+      try {
+        const res = await axios
+          .get("https://lengtith.onrender.com/api/users/user", {
+            withCredentials: true,
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': cookies.user,
+            }
+          });
+        setUser(res.name)
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    const getUserinfo = async () => {
+      try {
+        const res = await axios
+          .get("https://lengtith.onrender.com/api/users/info", {
+            withCredentials: true,
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': cookies.user,
+            }
+          })
+        setUserInfo(res.intro)
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    getUser();
+    getUserinfo();
+  }, [cookies])
+
+
   return (
-    <div>ProductUpdate</div>
+    <div>
+      {user}
+      {userInfo}
+    </div>
   )
 }
 
